f4070841886a14f6912186ba0fff61df13f5b532
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-30 17:21:38 +0100

    utilisation de la method use

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const app = express(); 
-const PORT = 3000
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-app.get('/movies', (req,res)=> {
-    res.send('Bientot des films ici même')
-})
+// Use permet de definir quel sont les midleware que nous voulons ajouter
+app.use('/public',express.static('public'))
 
-// Attention à l'ordre des routes 
-app.get('/movies/add',(req, res) => {
-    res.send(`Prochainement un formulaire d'ajout de film`)
-})
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-//Permet de recuperer un id et le lier à une page  
-app.get('/movies/:id', (req,res)=>{
-    const id = req.params.id
-    res.send(`Film n° ${id}`)
-})
-
-
-app.get('/',(req, res) => {
-    res.send('Hello world !')
+app.get("/movies", (req, res) => {
+  res.send("Bientot des films ici même");
 });
 
-    
+// Attention à l'ordre des Routes
+app.get("/movies/add", (req, res) => {
+  res.send(`Prochainement un formulaire d'ajout de film`);
+});
+
+//Permet de recuperer un id et le lier à une page
+app.get("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Film n° ${id}`);
+});
+
+app.get("/", (req, res) => {
+//   res.send("Hello world !");
+  res.render('index')
+});
+
 app.listen(PORT, () => {
-    console.log(`listen on port ${PORT}`);
+  console.log(`listen on port ${PORT}`);
 });
