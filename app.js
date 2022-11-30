05e6181aadddb967c292a2ecbf93f42fed289812
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-30 17:36:45 +0100

    creation des templates avec la method render

--- app.js
+++ app.js
@@ -3,29 +3,36 @@ const app = express();
 const PORT = 3000;
 
 // Use permet de definir quel sont les midleware que nous voulons ajouter
-app.use('/public',express.static('public'))
-
+app.use("/public", express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+// la method res render permet d'afficher du contenu .ejs à l'écran
 app.get("/movies", (req, res) => {
-  res.send("Bientot des films ici même");
+  //   res.send("Bientot des films ici même");
+  res.render("movies");
 });
 
+// app.get('/movies-details', (req,res)=>{
+//     res.render('movies-details')
+
+// })
+
 // Attention à l'ordre des Routes
+// La method send permet d'envoyer ce qui est contenue à l'interieur des () sur la page web designé
 app.get("/movies/add", (req, res) => {
-  res.send(`Prochainement un formulaire d'ajout de film`);
+  res.render('add-movies');
 });
 
 //Permet de recuperer un id et le lier à une page
 app.get("/movies/:id", (req, res) => {
   const id = req.params.id;
-  res.send(`Film n° ${id}`);
+  res.send('movies-details');
 });
 
 app.get("/", (req, res) => {
-//   res.send("Hello world !");
-  res.render('index')
+  //   res.send("Hello world !");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
