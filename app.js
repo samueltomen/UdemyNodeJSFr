3b6b1977b39749cb5162f39720faa37f3411db8d
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-30 11:01:39 +0100

    eddy is comming

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = 3000
 
 app.get('/',(req, res) => {
-    res.send('Hello world')
+    res.send('Hello world !')
 });
 
 app.listen(PORT, () => {
     console.log(`listen on port ${PORT}`);
 });
+
