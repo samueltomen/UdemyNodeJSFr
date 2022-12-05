17937693c73a565058f03d0025f65819eb4d5a41
Dytsaw <tomennana.samuel@outlook.fr>
2022-12-05 12:00:58 +0100

    gerer la memoire avec la method body-parser

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
+let frenchMovies = [];
+
 // Use permet de definir quel sont les midleware que nous voulons ajouter
 app.use("/public", express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 // la method res render permet d'afficher du contenu .ejs à l'écran
 app.get("/movies", (req, res) => {
   const title = "Film francais des 30 dernieres années ";
-  const frenchMovies = [
+  frenchMovies = [
     { title: `Le fabuleux destin d'amelie Poulain`, year: 2001 },
     { title: `Le fabuleux destin d'amelie Poulain`, year: 2101 },
     { title: `Le fabuleux destin d'amelie Poulain`, year: 2001 },
@@ -22,14 +24,21 @@ app.get("/movies", (req, res) => {
   res.render("movies", { movies: frenchMovies, title: title });
 });
 
-app.post("/movies", (req, res) => {
-  console.log(req.body);
+const urlencoderParser = bodyParser.urlencoded({ extended: false });
+
+app.post("/movies", urlencoderParser, (req, res) => {
+  console.log('Le titre est : ',req.body.movietitle)
+  console.log('L\'année est : ',req.body.movieyear);
+  const newMovie = { title : req.body.movietitle, year: parseInt(req.body.movieyear)};
+  // Utilisation du spread Operator
+  frenchMovies = [...frenchMovies, newMovie]
+  console.log(frenchMovies);
+
+  // Utilisatiton de la method push
+  // frenchMovie.push(newMovie)
+
+  res.sendStatus(201);
 });
-
-// app.get('/movies-details', (req,res)=>{
-//     res.render('movies-details')
-
-// })
 
 // Attention à l'ordre des Routes
 // La method send permet d'envoyer ce qui est contenue à l'interieur des () sur la page web designé
